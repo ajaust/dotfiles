b31b082e95ea9c3c8c08af4d3befe1f560d528c9
Alexander Jaust <aej@equinor.com>
2024-09-18 14:32:55 +0200

    Set up Copilot alognside snippets

--- nvim/lua/custom/plugins/copilot-cmp.lua
+++ nvim/lua/custom/plugins/copilot-cmp.lua
@@ -1,7 +1,18 @@
 -- Color scheme
 return {
+  event = "InsertEnter",
   "zbirenbaum/copilot-cmp",
   config = function ()
     require("copilot_cmp").setup()
-  end
+  end,
+  dependencies = {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end,
+  },
 }
