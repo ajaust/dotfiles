85dcb4a4d45ff6afdc81ed9ae07a51ea54c110f1
Alexander Jaust <18347253+ajaust@users.noreply.github.com>
2025-02-15 21:27:31 +0100

    Add scripts for backlight control

---
+++ sway/scripts/decrease-brightness.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+current_brightness=$(cat "/sys/class/backlight/amdgpu_bl1/brightness")
+max_brightness=$(cat "/sys/class/backlight/amdgpu_bl1/max_brightness")
+
+#echo $max_brightness
+step=$(echo "${max_brightness} / 20" | bc -l | xargs printf %.f)
+#echo $step
+new_value=$(echo "${current_brightness} - ${step}" | bc)
+new_value=$((${new_value} > 5 ? ${new_value}  : 5))
+echo "Setting display brightness to ${new_value}"
+
+echo ${new_value} > /sys/class/backlight/amdgpu_bl1/brightness
